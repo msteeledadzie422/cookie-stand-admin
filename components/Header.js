f1bc0243e6866dfeaf889ae4056184ae23a145a8
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2023-05-21 14:09:06 -0600

    added overview button

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,23 @@
-export default function Header({ answerCount }) {
-    return (
-        <header className="flex items-center justify-between p-4 bg-green-600 text-black">
-          <h1 className="text-4xl">Cookie Stand Admin</h1>
-        </header>
-    );
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+export default function Header() {
+  const router = useRouter();
+
+  const handleNavigateToMainPage = () => {
+    router.push('/');
+  };
+
+  return (
+    <header className="flex items-center justify-between p-4 bg-green-600 text-black">
+      <h1 className="text-4xl" onClick={handleNavigateToMainPage} style={{ color: 'black' }}>
+        Cookie Stand Admin
+      </h1>
+      <div className="bg-gray-200 px-4 py-2 rounded-md">
+        <Link href="/overview">
+          <button className="text-black font-bold" style={{ color: 'black' }}>Overview</button>
+        </Link>
+      </div>
+    </header>
+  );
 }
---
+++ pages/overview.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+export default function Overview() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <p>Hello World</p>
+      </main>
+      <Footer />
+    </div>
+  );
+}
